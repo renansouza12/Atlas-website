fc40ce8c04abd31acf169411f716a17f0712d6b4
Renan Souza <renansouza0122@gmail.com>
2023-12-04 21:01:34 -0200

    header and title added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/sections/hero/hero.component';
+import { HeaderComponent } from './components/sections/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/sections/hero/hero.component.ts
@@ -0,0 +1,22 @@
+import { Component,OnInit } from '@angular/core';
+import { gsap } from "gsap";
+import SplitType from 'split-type'
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss','hero.responsive.component.scss']
+})
+export class HeroComponent implements OnInit {
+  ngOnInit(): void {
+    const myTitle = new SplitType(".title");
+
+    gsap.from('.char',{
+      opacity:0,
+      stagger:.2,
+      delay:.5,
+      duration: .5,
+      y:100
+    })
+  }
+
+}
